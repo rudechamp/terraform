2b18b147460b4ae0f383af473d6c542c61c4b0d5
Dmytro Kozachko <dmytro.kozachko@gmail.com>
2021-12-13 12:44:38 +0000

    Adding WPLB

---
+++ WordpressLB/files/userdata.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+sudo apt -y update
+sudo apt -y upgrade
+sudo apt install -y nginx
+sudo apt install -y mysql
+sudo apt install -y php7.2 php7.2-cli php7.2-fpm php7.2-mysql php7.2-json php7.2-opcache php7.2-mbstring php7.2-xml php7.2-gd php7.2-curl
+sudo service php restart
+sudo service nginx restart
+sudo mkdir -p /var/www/html/wordpress/public_html
+sudo cd /var/www/html/wordpress/public_html
+wget -c https://wordpress.org/wordpress-5.8.2.tar.gz
+tar -xzvf wordpress-5.8.2.tar.gz
+sudo cd /var/www/html/wordpress/public_html
+sudo chown -R www-data:www-data *
+sudo chmod -R 755 *
+sudo cp /var/www/html/wp-config-sample.php /var/www/html/wordpress/public_html/wp-config.php
+sudo cp /tmp/wordpress.conf /etc/nginx/sites-available/wordpress.conf
+sudo cd /etc/nginx/sites-enabled
+sudo ln -s ../sites-available/wordpress.conf .
+sudo service nginx restart
